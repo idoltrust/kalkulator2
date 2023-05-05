472d67bc97cc362f41f6126edea77f4fc1b4a589
kirill <basdania123123@gmail.com>
2023-05-05 14:31:48 +0300

    old version

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tViev:TextView = findViewById(R.id.text)
+        val add:Button = findViewById(R.id.Add)
+        val subtr:Button = findViewById(R.id.subtr)
+        val mul:Button = findViewById(R.id.mul)
+        val divide:Button = findViewById(R.id.divide)
+
+        add.setOnClickListener { tViev.text = but(arg1(),arg2()).add() }
+        subtr.setOnClickListener { tViev.text = but(arg1(),arg2()).add()}
+        mul.setOnClickListener {tViev.text = but(arg1(),arg2()).add()}
+        divide.setOnClickListener {tViev.text = but(arg1(),arg2()).add()}
+        }
+
+    fun arg1 ():Int {
+        val Arg1:EditText = findViewById(R.id.editText1)
+        val Arg1_ = Arg1.getText().toString().toInt()
+        return Arg1_
+    }
+
+    fun arg2 ():Int {
+        val Arg2:EditText = findViewById(R.id.editText2)
+        val Arg2_ = Arg2.getText().toString().toInt()
+        return Arg2_
+    }
+}
+
---
+++ app/src/main/java/com/example/myapplication/but.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+import android.widget.EditText
+
+class but(var Arg1_:Int = 0, var Arg2_ :Int = 0){
+    fun add ():String{
+        val otv:Int = Arg1_+Arg2_
+        return "$otv"
+    }
+    fun subtr ():String{
+        var otv:Int = Arg1_-Arg2_
+        return "$otv"
+    }
+    fun mul ():String{
+        var otv:Int = Arg1_*Arg2_
+        return "$otv"
+    }
+    fun divide ():String{
+        if(Arg2_ == 0){
+            return "Div by zero"
+        }
+        else{val otv:Int = Arg1_/Arg2_
+            return "$otv"
+        }
+    }
+}
